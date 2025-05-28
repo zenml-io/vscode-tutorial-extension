257dc462ff73f0f317f39f505bb806c6c6c23149
Safoine El khabich <safoinme@gmail.com>
2025-05-28 14:16:34 +0100

    update extension uri for codespcae

--- src/utils/getExtensionUri.ts
+++ src/utils/getExtensionUri.ts
@@ -2,13 +2,23 @@ import * as vscode from "vscode";
 
 export default function getExtensionUri(context: vscode.ExtensionContext) {
   if (context.extensionMode === vscode.ExtensionMode.Production) {
+    // Check if ZENML_ENABLE_TUTORIAL is set to true (for codespace containers)
+    const isTutorialEnabled = process.env.ZENML_ENABLE_TUTORIAL === "true";
     const isCodespace = process.env.CODESPACES === "true";
 
-    return vscode.Uri.file(
-      `/root/${
-        isCodespace ? ".vscode-remote" : ".vscode-server"
-      }/extensions/zenml.zenml-codespace-tutorial-0.0.1/pipelines`
-    );
+    if (isTutorialEnabled) {
+      // Use the codespace container extension path when tutorial is enabled
+      return vscode.Uri.file(
+        "/opt/code-server/extensions/zenml.zenml-codespace-tutorial-0.0.1/pipelines"
+      );
+    } else {
+      // Use the original logic for other environments
+      return vscode.Uri.file(
+        `/root/${
+          isCodespace ? ".vscode-remote" : ".vscode-server"
+        }/extensions/zenml.zenml-codespace-tutorial-0.0.1/pipelines`
+      );
+    }
   } else {
     return vscode.Uri.file("/workspaces/vscode-tutorial-extension/pipelines");
   }
